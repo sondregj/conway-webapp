e12b58085ff98e3800408731e338dda6cb71e0a6
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-09-29 18:58:26 +0200

    :sparkles: Added index file for components

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+// Top level components
+export { default as Footer } from './Footer'
+
+// Inputs
+export { default as Button } from './Button'
+export { default as RangeSlider } from './RangeSlider'
+
+// Game of Life
+export { default as GOLBoard } from './GOLBoard'
+export { default as GOLRow } from './GOLRow'
+export { default as GOLCell } from './GOLCell'
